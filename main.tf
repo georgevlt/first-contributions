bfc038ceaf53a61a2194db5a2cdfeaae152075ca
georgevlt <babii.georgeandrei@yahoo.com>
2022-08-29 17:29:37 +0300

    added new ec2 on main.tf

--- main.tf
+++ main.tf
@@ -30,3 +30,12 @@ resource "aws_vpc" "SCC" {
     cidr_block = "10.0.0.26/15"
 
 }
+
+# Create a new host with instance type of c5.18xlarge with Auto Placement
+# and Host Recovery enabled.
+resource "aws_ec2_host" "test" {
+  instance_type     = "c5.18xlarge"
+  availability_zone = "eu-east"
+  host_recovery     = "on"
+  auto_placement    = "on"
+}
