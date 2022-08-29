a214730ad43d1150bac9c3fe0044c4591451de7f
georgevlt <babii.georgeandrei@yahoo.com>
2022-08-29 14:34:54 +0300

    changes main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-export AWS_ACCESS_KEY_ID="123456789"
-export AWS_SECRET_ACCESS_KEY="123456789"
-export AWS_REGION="us-west-2"
+export AWS_ACCESS_KEY_ID="george"
+export AWS_SECRET_ACCESS_KEY="george"
+export AWS_REGION="eu-north-2"
 
 terraform {
   required_providers {
@@ -13,11 +13,11 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-north-2"
 }
 
 # Create a VPC
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "20.0.0.0/16"
 }
 
