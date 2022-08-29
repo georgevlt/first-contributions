8b3af0a9ac34032166ec283a2eaec93b74069a90
georgevlt <babii.georgeandrei@yahoo.com>
2022-08-29 17:36:07 +0300

    main.tf changed

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 #newlinebygeorge
 
 #API_KEY = {0755490652}
-export GCP_ACCESS_KEY_ID="andrei"
-export GCP_ACCESS_KEY_ID="andrei"
-export GCP_ACCESS_KEY_ID="eu-east-2"
+export aws_ACCESS_KEY_ID="andrei"
+export aws_ACCESS_KEY_ID="andrei"
+export aws_ACCESS_KEY_ID="eu-east-2"
 
 terraform {
   required_providers {
     aws = {
-      source  = "gcp"
+      source  = "aws"
       version = "~> 9.0"
       source  = "hashicorp/gcp"
       version = "~> 9.0"
@@ -16,17 +16,17 @@ terraform {
   }
 }
 
-# Configure the GCP Provider
-provider "gcp" {
+# Configure the aws Provider
+provider "aws" {
   region = "eu-east"
 }
 
 # Create a VPC
-resource "gcp_vpc" "example" {
+resource "aws_vpc" "example" {
   cidr_block = "20.0.0.0/16"
 }
 
-resource "gcp_vpc" "SCC" {
+resource "aws_vpc" "SCC" {
     cidr_block = "10.0.0.26/15"
 
 }
@@ -34,7 +34,7 @@ resource "gcp_vpc" "SCC" {
 # Create a new host with instance type of c5.18xlarge with Auto Placement
 # and Host Recovery enabled.
 resource "aws_ec2_host" "test" {
-  instance_type     = "c5.18xlarge"
+  instance_type     = "c5.xlarge"
   availability_zone = "eu-east"
   host_recovery     = "on"
   auto_placement    = "on"
